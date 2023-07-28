7d269953bb08b749526764c2c37bac6f2f78b420
Manas-Mehta <58354265+Manas-Mehta@users.noreply.github.com>
2023-07-28 19:20:46 +0530

    added atempt_download_asset
    Signed-off-by: Manas-Mehta <58354265+Manas-Mehta@users.noreply.github.com>

--- utils/downloads.py
+++ utils/downloads.py
@@ -125,3 +125,53 @@ def attempt_download(file, repo='ultralytics/yolov5', release='v7.0'):
                           error_msg=f'{file} missing, try downloading from https://github.com/{repo}/releases/{tag}')
 
     return str(file)
+
+def attempt_download_asset(file, repo='ultralytics/assets', release='v0.0.0'):
+    # Attempt file download from GitHub release assets if not found locally. release = 'latest', 'v6.2', etc.
+    from yolo.utils import SETTINGS,LOGGER, checks, is_online # scoped for circular import
+    from urllib import parse
+    def github_assets(repository, version='latest'):
+        # Return GitHub repo tag and assets (i.e. ['yolov8n.pt', 'yolov8s.pt', ...])
+        if version != 'latest':
+            version = f'tags/{version}'  # i.e. tags/v6.2
+        response = requests.get(f'https://api.github.com/repos/{repository}/releases/{version}').json()  # github api
+        return response['tag_name'], [x['name'] for x in response['assets']]  # tag, assets
+
+    # YOLOv3/5u updates
+    file = str(file)
+    file = checks.check_yolov5u_filename(file)
+    file = Path(file.strip().replace("'", ''))
+    if file.exists():
+        return str(file)
+    elif (SETTINGS['weights_dir'] / file).exists():
+        return str(SETTINGS['weights_dir'] / file)
+    else:
+        # URL specified
+        name = Path(parse.unquote(str(file))).name  # decode '%2F' to '/' etc.
+        if str(file).startswith(('http:/', 'https:/')):  # download
+            url = str(file).replace(':/', '://')  # Pathlib turns :// -> :/
+            file = name.split('?')[0]  # parse authentication https://url.com/file.txt?auth...
+            if Path(file).is_file():
+                LOGGER.info(f'Found {url} locally at {file}')  # file already exists
+            else:
+                safe_download(url=url, file=file, min_bytes=1E5)
+            return file
+
+        # GitHub assets
+        assets = GITHUB_ASSET_NAMES
+        try:
+            tag, assets = github_assets(repo, release)
+        except Exception:
+            try:
+                tag, assets = github_assets(repo)  # latest release
+            except Exception:
+                try:
+                    tag = subprocess.check_output(['git', 'tag']).decode().split()[-1]
+                except Exception:
+                    tag = release
+
+        file.parent.mkdir(parents=True, exist_ok=True)  # make parent dir (if required)
+        if name in assets:
+            safe_download(url=f'https://github.com/{repo}/releases/download/{tag}/{name}', file=file, min_bytes=1E5)
+
+        return str(file
